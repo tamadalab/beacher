d11fbeb7a72405b1d9e50028c8c455f22fa77b19
J.Nishi <98422335+ji-ua@users.noreply.github.com>
2022-11-02 12:34:47 +0900

    BuildToolDefクラスを作成、およびメソッドなどのスケルトンコードを記述

--- app/src/main/java/example/Beacher.java
+++ app/src/main/java/example/Beacher.java
@@ -1,5 +1,26 @@
 package example;
 
-public class Beacher {
-    
+import java.util.Vector;
+import java.nio.file.Path;
+
+public class Beacher extends Object
+{
+    Vector<BuildToodDef> BuildToolDefs = new Vector<BuildToodDef>();
+
+    public Vector<BuildToodDef> parse(Path path)
+    {
+        return "hoge";//修正する
+    }
+
+    public Vector<BuildToodDef> parse_from_asset()
+    {
+        return "hoge";//修正する
+    }
+
+    public Vector<BuildToodDef> Construct(Path defs,Path append)
+    {
+        return "hoge";//修正する
+    }
+
+
 }
---
+++ app/src/main/java/example/BuildToodDef.java
@@ -0,0 +1,9 @@
+package example;
+
+import java.util.Vector;
+public class BuildToodDef
+{
+    public String name;
+    public Vector<String> build_files;
+    public String url;
+}
