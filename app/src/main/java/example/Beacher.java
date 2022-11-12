7ffab03c34905de95b42dc0c2d3e9b75ac9c1a50
J.Nishi <98422335+ji-ua@users.noreply.github.com>
2022-11-12 16:07:12 +0900

    Beacher.javaをある程度変換。BuildToolDef.javaのスケルトンコード作成。

--- app/src/main/java/example/Beacher.java
+++ app/src/main/java/example/Beacher.java
@@ -1,25 +1,38 @@
 package example;
 
-import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
 import java.nio.file.Path;
 
-public class Beacher extends Object
+public class Beacher extends BuildToodDef
 {
-    Vector<BuildToodDef> BuildToolDefs = new Vector<BuildToodDef>();
-
-    public Vector<BuildToodDef> parse(Path path)
+    public List<BuildToodDef> merge_build_tools(List<BuildToodDef> first, List<BuildToodDef> second)
     {
-        return "hoge";//修正する
+        List<BuildToodDef> result = new ArrayList<BuildToodDef>();
+        for (BuildToodDef item : first) result.add(item);
+        for (BuildToodDef item : second) result.add(item);
+
+        return result;
     }
 
-    public Vector<BuildToodDef> parse_from_asset()
+    public List<BuildToodDef> Construct(Path defs,Path append)
     {
-        return "hoge";//修正する
-    }
+        List<BuildToodDef> def = new ArrayList<BuildToodDef>();
+        if(!(defs==null)) def = parse(defs);
+        else def = parse_from_asset();
 
-    public Vector<BuildToodDef> Construct(Path defs,Path append)
-    {
-        return "hoge";//修正する
+        List<BuildToodDef> result = new ArrayList<BuildToodDef>();
+        if (!(defs==null))
+        {
+            List<BuildToodDef> additional_defs = parse(append);
+            result = merge_build_tools(def, additional_defs);
+        }
+        else
+        {
+            result = def;
+        }
+
+        return result;
     }
 
 
--- app/src/main/java/example/BuildToodDef.java
+++ app/src/main/java/example/BuildToodDef.java
@@ -1,9 +1,24 @@
 package example;
 
-import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
+import java.nio.file.Path;
+
 public class BuildToodDef
 {
     public String name;
-    public Vector<String> build_files;
+    public List<String> build_files;
     public String url;
+
+
+    public List<BuildToodDef> parse(Path path)
+    {
+        
+        
+    }
+
+    public List<BuildToodDef> parse_from_asset()
+    {
+
+    }
 }
