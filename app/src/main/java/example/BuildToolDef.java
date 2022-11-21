d63f4b34d6e132215582558d71fa8105685ad1dc
J.Nishi <98422335+ji-ua@users.noreply.github.com>
2022-11-21 15:32:43 +0900

    jsonファイルをListに変換する処理を実装

--- app/src/main/java/example/BuildToolDef.java
+++ app/src/main/java/example/BuildToolDef.java
@@ -2,34 +2,33 @@ package example;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.nio.file.Path;
 
+import java.nio.file.Path;
 import java.io.File;
 import java.io.IOException;
  
-
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class BuildToolDef
 {
     public String name;
+
+    @JsonProperty("build-files")
     public List<String> build_files;
+
     public String url;
 
-
-    public List<BuildToolDef> parse(Path path)
+    public List<BuildToolDef> parse(Path path) throws IOException
     {
         ObjectMapper objectMapper = new ObjectMapper();
-        File buildtools = new File(path.toString());
-
-        return objectMapper.readValue(buildtools, new TypeReference<List<BuildToolDef>>());
-
+        return objectMapper.readValue(new File(path.toString()), new TypeReference<List<BuildToolDef>>());
     }
 
     public List<BuildToolDef> parse_from_asset()
     {
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new File("buildtools.json"),new TypeReference<List<BuildToolDef>>(){});
     }
 }
