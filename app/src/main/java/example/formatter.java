013cca987dcf3011440f16d322afe3e8614bfbdb
小林　巧明 <kito@tanuki.local>
2022-11-24 15:13:38 +0900

    　多分完成した

--- app/src/main/java/example/formatter.java
+++ app/src/main/java/example/formatter.java
@@ -2,12 +2,21 @@ package Beacher;
 
 import java.awt;
 import java.util.function.UnaryOperator;
+import java.util.ArrayList;
+import java.util.List;
 
-interface Formatter {
-    ArrayList<String> DefaultFormatter = new ArrayList<>();
-    ArrayList<String> JsonFormatter = new ArrayList<>();
-    ArrayList<String> XmlFormatter = new ArrayList<>();
-    ArrayList<String> YamlFormatter = new ArrayList<>();
+import javafx.scene.image.Image;
+
+public interface Formatter {
+
+    List<String> DefaultFormatter = new ArrayList<>();
+    List<String> JsonFormatter = new ArrayList<>();
+    List<String> XmlFormatter = new ArrayList<>();
+    List<String> YamlFormatter = new ArrayList<>();
+    List<integer> Item = new ArrayList<>();
+    List Print_Head = new ArrayList<>();
+    List<BuildTooldef> def = new ArrayList<>();
+    // List <BuildTooldefs> defs = new ArrayList<>();
 
     public String print_header;
     public String print_defs_header;
@@ -15,27 +24,49 @@ interface Formatter {
     public String print_defs_footer;
     public String print_def;
     public String print_each;
+    public int item;
+    public Path base;// インスタンスいる
 
+    default String Item() {
+        if (item == 0) {
+            item = 0;
+        } else {
+            item = 1;
+        }
+        return print_each;
+    }
+
+    default String Match() {
+        if (Format == Json) {
+            return JsonFormatter;
+        } else if (Format == Default) {
+            return DefaultFormatter;
+        } else if (Format == Xml) {
+            return XmlFormatter;
+        } else if (Format == Yaml) {
+            return YamlFormatter;
+        }
+    }
 }
 
 class DefaultFormatter implements Formatter {
 
-    public void print_header() {
-            System.out.println("%s" base.display);//ヘッダー
+    public void print_header(Path base, BuildTooldef def) {
+        System.out.println("%s"this.base);// thisのところにもらってきたクラス名を入れると動くはず
+    }
+
+    public void print_each(Path base, BuildTooldef def) {
+            System.out.println("%s:" this.base.display, this.def.name);//いーち
         }
 
-    public void print_each() {
-            System.out.println("%s:" base.display, def.name);//いーち
-        }
-
-    public void print_def() {
-            System.out.println("%s: %s" def.name, def.build_files.join(","));//def
+    public void print_def(Path base, BuildTooldef def) {
+            System.out.println("%s: %s" this.def.name, this.def.build_files.join(","));//def
         }
 }
 
 class JsonFormatter implements Formatter {
 
-    public void print_header() {
+    public void print_header(Path base) {
         System.out.println("base:%s,build-tools:[", base.display()).unwrap();// header
     }
 
@@ -53,7 +84,7 @@ class JsonFormatter implements Formatter {
 }
 
 class XmlFormatter implements Formatter {
-    public void print_header() {
+    public void print_header(Path base) {
         System.out.println("<?xml version=\"1.0\"?>");// header
         System.out.println("<build-tools><base>%s</base>", base.display);
     }
@@ -62,7 +93,7 @@ class XmlFormatter implements Formatter {
         System.out.println("</build-tools>");// footer
     }
 
-    public void print_each(){
+    public void print_each(BuildTooldef def){
              System.out.println("<build-tool><file-path>%s</file-path><tool-name>%s</tool-name></build-tool>"result.path.display(),def.name);//each
         }
 
@@ -75,18 +106,18 @@ class XmlFormatter implements Formatter {
         System.out.println("</build-tool-defs>");// def_fotter
     }
 
-    public void print_def() {
+    public void print_def(BuildTooldef def) {
         System.out.println("<build-tool-def><name>%s</name><url>%s</url><build-files>", def.name, def.url);// print_def
     }
 }
 
 class YamlFormatter implements Formatter {
 
-    public void print_header() {
+    public void print_header(Path base) {
         System.out.println("base: %s", base.display);// print_header
     }
 
-    public void print_each() {
+    public void print_each(Path base, BuildTooldef def) {
         System.out.println("  - file-path: %s", result.path.display());// print_each
         System.out.println("    tool-name: %s", result.def.name);
     }
@@ -95,23 +126,37 @@ class YamlFormatter implements Formatter {
         System.out.println("build-tools-defs");// print_def_header
     }
 
-    public void print_def() {
+    public void print_def(BuildTooldef def) {
         System.out.println("  - name: %s", def.name);// print_def
         System.out.println("    url: %s", def.url);
         System.out.println("    file-names:");
         // ここからeachのやつ使う
     }
-}
 
-public class Main(){
- switch(
-        case  JsonFormatter;
-        return Jsonformatter;
-        break;
-        case Default;
-        return DefaultFormatter;
-        break;
+    public void print_file_name() {
+        int index;
+        str file_name;
+        if (index == 0) {
+            System.out.print("      -");
+        } else {
+            System.out.print("       ");
+        }
+        System.out.println("%d", file_name);
+    }
 
-       )
+    public static void main(String[] args) {
 
+        /*
+         * public void Formatter(){
+         * ArrayList Print_Head = new ArrayList<>();
+         * int item;
+         * if(item == 0){
+         * item = 0;
+         * }else{
+         * item = 1;
+         * }
+         * return print_each;
+         * }
+         */
+    }
 }
---
+++ app/src/main/java/example/formatterTest.java
@@ -0,0 +1,14 @@
+package formatter.junit5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+public class formatterTest {
+
+Formatter formatter = new Formatter();
+
+    @Test
+    void f(){
+
+    }
+}
