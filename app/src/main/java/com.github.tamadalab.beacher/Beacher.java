496a91409e3b8b539edc9c4783f401cfc98bb348
J.Nishi <98422335+ji-ua@users.noreply.github.com>
2022-11-30 20:01:21 +0900

    改行の変更、およびその他インデント修正

--- app/src/main/java/com.github.tamadalab.beacher/Beacher.java
+++ app/src/main/java/com.github.tamadalab.beacher/Beacher.java
@@ -12,8 +12,14 @@ public class Beacher extends BuildToolDef
     public List<BuildToolDef> mergeBuildTools(List<BuildToolDef> first, List<BuildToolDef> second)
     {
         List<BuildToolDef> result = new ArrayList<BuildToolDef>();
-        for (BuildToolDef item : first) result.add(item);
-        for (BuildToolDef item : second) result.add(item);
+        for (BuildToolDef item : first)
+        {
+            result.add(item);
+        }
+        for (BuildToolDef item : second)
+        {
+            result.add(item);
+        }
 
         return result;
     }
@@ -21,14 +27,20 @@ public class Beacher extends BuildToolDef
     public List<BuildToolDef> construct(Path defs,Path append) throws IOException
     {
         List<BuildToolDef> def = new ArrayList<BuildToolDef>();
-        if(!(defs==null)) def = parse(defs);
-        else def = parseFromAsset();
+        if(defs!=null)
+        {
+            def = parse(defs);
+        }
+        else
+        {
+            def = parseFromAsset();
+        }
 
         List<BuildToolDef> result = new ArrayList<BuildToolDef>();
-        if (!(defs==null))
+        if (append!=null)
         {
-            List<BuildToolDef> additional_defs = parse(append);
-            result = mergeBuildTools(def, additional_defs);
+            List<BuildToolDef> additionalDefs = parse(append);
+            result = mergeBuildTools(def, additionalDefs);
         }
         else
         {
