3e319fe339e35167c2b4f8f1adbae0e0b2378259
山崎駿 <yamazakishun@yamazakishunnoMacBook-Air.local>
2022-12-01 17:17:27 +0900

    BuildToolDef.javano

---
+++ app/src/main/java/example/BuildToolDef.java
@@ -0,0 +1,59 @@
+// package com.github.tamadalab.beacher;
+package example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import java.nio.file.Path;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BuildToolDef
+{
+    public String name;
+
+    @JsonProperty("build-files")
+    public List<String> buildFiles;
+
+    public String url;
+
+    public List<BuildToolDef> parse(Path path)
+    {
+        try
+        {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(new File(path.toString()), new TypeReference<List<BuildToolDef>>(){});
+        }
+        catch (IOException anException)
+        {
+            anException.printStackTrace();
+            return new ArrayList<>();
+        }
+
+    }
+
+    public List<BuildToolDef> parseFromAsset()
+    {
+        try
+        {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(new File("defs/buildtools.json"),new TypeReference<List<BuildToolDef>>(){});
+        }
+        catch (FileNotFoundException anException)
+        {
+            System.out.println("buildtools.json: file not found");
+            return new ArrayList<>();
+        }
+        catch (IOException anException)
+        {
+            anException.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+}
