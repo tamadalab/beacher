5d94381fb58b6b7bbc9eb0f1d7d4f534ae0e260c
吉田大介 <daisuke@yoshidadaisukenoMacBook-Pro.local>
2022-12-09 09:34:37 +0900

    testcodeの作成

--- app/src/test/java/com.github.tamadalab.beacher/CliTest.java
+++ app/src/test/java/com.github.tamadalab.beacher/CliTest.java
@@ -1,2 +1,16 @@
-package com.github.tamadalab.beacher;public class CliTest {
+package com.github.tamadalab.beacher;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+
+public class CliTest {
+
+    @Test
+    void validate() throws Exception{
+        Cli aCli = new Cli();
+        //aCli.projectList = (".");
+        aCli.dirs.add(Path.of("."));
+        aCli.validate();
+    }
 }
