e602fe484fbd33e16a3c225a40f7b7d4da3c45df
山崎駿 <yamazakishun@yamazakishunnoMacBook-Air.local>
2022-12-09 22:40:58 +0900

    テスト内容の追加

--- app/src/test/java/com.github.tamadalab.beacher/ExampleTest.java
+++ app/src/test/java/com.github.tamadalab.beacher/ExampleTest.java
@@ -1,6 +1,7 @@
 package com.github.tamadalab.beacher;
 
 import java.nio.file.Path;
+import java.util.Formatter;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,18 @@ public class ExampleTest {
     public void testFindBuildFiles() throws Exception {
         Beacher beacher = new Beacher();
         Example example = new Example();
+
         var defs = beacher.construct(Path.of("../defs/buildtools.json"), null);
-        example.performEach(Path.of("."), defs, false, null);
+        example.performEach(Path.of("."), defs, false);
+    }
+    @Test
+    public void testBuildFormatter() throws Exception {
+        Formatter formatter = new Formatter();
+        Cli opts = new Cli();
+        Example example = new Example();
+        Beacher beacher = new Beacher();
+        var defs = beacher.construct(Path.of("../defs/buildtools.json"), null);
+        var result = example.performEach(Path.of("."), defs, false);
+        example.definitionPrint(opts, defs, Path.of("."), result);
     }
 }
