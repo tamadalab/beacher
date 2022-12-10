e9c9cac8f7b9d802d451db62d2c95e381f888c35
J.Nishi <98422335+ji-ua@users.noreply.github.com>
2022-12-11 02:24:27 +0900

    testConstruct()の実装

--- app/src/test/java/com.github.tamadalab.beacher/BeacherTest.java
+++ app/src/test/java/com.github.tamadalab.beacher/BeacherTest.java
@@ -1,12 +1,22 @@
 package com.github.tamadalab.beacher;
 
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-public class BeacherTest {
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BeacherTest
+{
     @Test
-    public void testConstruct()
+    public void testConstruct() throws Exception
     {
+        Beacher aBeacher = new Beacher();
+        BuildToolDef aBuildToolDef = new BuildToolDef();
 
+        List<BuildToolDef> constructDefs = aBeacher.construct(Path.of("../defs/buildtools_sample.json"), Path.of("../defs/buildtools_append_sample.json"));
+        
+        assertTrue(constructDefs.size() == 24);
     }
 }
