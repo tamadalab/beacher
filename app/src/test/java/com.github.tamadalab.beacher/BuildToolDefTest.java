831f4bab7b7549f7265ad43878d7c19c771b18c5
J.Nishi <98422335+ji-ua@users.noreply.github.com>
2022-12-11 02:26:08 +0900

    testParse()、testDefinition()の実装

--- app/src/test/java/com.github.tamadalab.beacher/BuildToolDefTest.java
+++ app/src/test/java/com.github.tamadalab.beacher/BuildToolDefTest.java
@@ -1,8 +1,36 @@
 package com.github.tamadalab.beacher;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.file.Path;
+import java.util.List;
 
-public class BuildToolDefTest {
-    
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BuildToolDefTest
+{
+    @Test
+    public void testParse() throws Exception
+    {
+        BuildToolDef aBuildToolDef = new BuildToolDef();
+        List<BuildToolDef> defs = aBuildToolDef.parse(Path.of("../defs/buildtools.json"));
+        assertTrue(defs.size() == 24);
+        //assertTrue(defs.size() == 0);
+    }
+
+    @Test
+    public void testDefinition() throws Exception
+    {
+        BuildToolDef aBuildToolDef = new BuildToolDef();
+        List<BuildToolDef> defs = aBuildToolDef.parse(Path.of("../defs/buildtools_sample.json"));
+        assertTrue(defs.size() == 12);
+        //assertTrue(defs.size() == 0);
+    }
+
+    // @Test
+    // public void testParseFromAsset() throws Exception
+    // {
+    //     BuildToolDef aBuildToolDef = new BuildToolDef();
+    //     List<BuildToolDef> defs = aBuildToolDef.parseFromAsset();
+    //     assertTrue(defs.size() == 24);
+    // }
 }
