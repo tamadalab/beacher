5234aeab0af282d2d873b4762ffbef14799a6f69
山崎駿 <yamazakishun@yamazakishunnoMacBook-Air.local>
2022-12-15 16:45:04 +0900

    definitionPrintメソッド一部変更

--- app/src/main/java/com.github.tamadalab.beacher/Example.java
+++ app/src/main/java/com.github.tamadalab.beacher/Example.java
@@ -149,6 +149,10 @@ public class Example extends Object
         {
             aFormatter.printDefs(defs);
         }
+        else
+        {
+            aFormatter.print(target, result);
+        }
     }
     public Path extractTarget(List<Path> targets)
     {
